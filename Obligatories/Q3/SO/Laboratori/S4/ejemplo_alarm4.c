63fa1103007326a3d37172eeb06bcd4a5460c46e
d1mo22 <davidmc04and@gmail.com>
2023-10-21 13:19:59 +0200

    21/10/23

--- Obligatories/Q3/SO/Laboratori/S4/ejemplo_alarm4.c
+++ Obligatories/Q3/SO/Laboratori/S4/ejemplo_alarm4.c
@@ -5,31 +5,25 @@
 #include <string.h>
 #include <sys/types.h>
 
-void
-error_y_exit (char *msg, int exit_status)
-{
+void error_y_exit (char *msg, int exit_status) {
   perror (msg);
   exit (exit_status);
 }
 
 int segundos = 0;
-void
-funcion_alarma (int signal)
-{
+
+void funcion_alarma (int signal) {
   char buff[256];
   segundos = segundos + 10;
   sprintf (buff, "ALARMA pid=%d: %d segundos\n", getpid (), segundos);
   write (1, buff, strlen (buff));
 }
 
-int
-main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
   struct sigaction sa;
   sigset_t mask;
 
   /* EVITAMOS TRATAR EL SIGALRM FUERA DEL SIGSUSPEND */
-
   sigemptyset (&mask);
   sigaddset (&mask, SIGALRM);
   sigprocmask (SIG_BLOCK, &mask, NULL);
@@ -39,23 +33,21 @@ main (int argc, char *argv[])
   sa.sa_flags = SA_RESTART;
   sigfillset (&sa.sa_mask);
 
-  if (sigaction (SIGALRM, &sa, NULL) < 0)
-    error_y_exit ("sigaction", 1);
+  if (sigaction (SIGALRM, &sa, NULL) < 0) error_y_exit ("sigaction", 1);
 
-  switch (fork ())
-    {
+  switch (fork ()) {
     case -1:
       error_y_exit ("fork", 1);
     case 0:
       alarm (10);
-    }
-  while (segundos < 100)
-    {
-      sigfillset (&mask);
-      sigdelset (&mask, SIGALRM);
-      sigdelset (&mask, SIGINT);
-      sigsuspend (&mask);
-      alarm (10);
-    }
+  }
+
+  while (segundos < 100) {
+    sigfillset (&mask);
+    sigdelset (&mask, SIGALRM);
+    sigdelset (&mask, SIGINT);
+    sigsuspend (&mask);
+    alarm (10);
+  }
   exit (1);
 }

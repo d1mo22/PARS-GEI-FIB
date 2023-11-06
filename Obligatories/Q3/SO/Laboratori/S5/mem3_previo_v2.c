66db815f9c42b784c25b5d2e585fa369b2b097ba
d1mo22 <davidmc04and@gmail.com>
2023-11-06 10:20:59 +0100

    06/11/23

--- Obligatories/Q3/SO/Laboratori/S5/mem3_previo_v2.c
+++ Obligatories/Q3/SO/Laboratori/S5/mem3_previo_v2.c
@@ -2,19 +2,30 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include<unistd.h>
 
-#define REGION_SIZE		4096
+#define REGION_SIZE	4096
+
+void trat_sig(int s) {
+  perror("Has accedido a una posicion de memoria no permitida\n");
+  exit(2);
+}
 
 int *p;
 
-int
-main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
+  struct sigaction sa;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sa.sa_handler = trat_sig;
+  sigaction(SIGSEGV, &sa, NULL);
+
   char buff[256];
 
   sprintf (buff, "Addresses:\n");
   write (1, buff, strlen (buff));
+
   sprintf (buff, "\tp: %p\n", &p);
   write (1, buff, strlen (buff));
 

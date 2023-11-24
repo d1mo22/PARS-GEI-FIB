5a21b0fc4dc0f55a3af514489be746b3d9a96d0d
d1mo22 <davidmc04and@gmail.com>
2023-11-25 00:06:15 +0100

    25/11/23

--- Obligatories/Q3/EDA/Joc/AIDM30.cc
+++ Obligatories/Q3/EDA/Joc/AIDM30.cc
@@ -17,6 +17,37 @@ struct PLAYER_NAME : public Player {
   static Player* factory () {
     return new PLAYER_NAME;
   }
+  /*
+    - Pioneers Exploradores | 50hp | 15 unidades | MAXIMO 60
+
+    - Furyans Guerrreros | 100hp | 5 unidades | Atk 25-50 | MAXIMO 20
+        NO pueden conquistar ni coger gemas
+
+    - Hellhounds (bajotierra) | Inmortal | 3 unidades | Instakill (alrededor) |
+        Siempre se acercan al explorador/guerrero mas cercano
+
+    - Necromonger (superficie) | 75hp | Atk 20-40
+        Spawnean con un 50% prob cada ronda en las celdas que acaban de volverse noche
+          
+    - Al principio de CADA ronda se RECUPERAN 5hp de todas las unidades
+    - Cada celda puede tener una unidad, si una entra en el lugar de una se considera ataque
+     
+    - Cada ronda solo la primera orden sera usada
+        Si se da mas de 1000 ordenes el la misma ronda ABORTA
+
+    - El orden de accion de los jugadores es aleatorio, pero se guarda el orden de las unidades
+
+    - Al matar exploradores/guerreros estos spawnean en un lugar random y pertenecen al grupo
+      que los ha mantado. 
+      A NO SER QUE los mate la maquina ENTONCES spawnean en un equipo random.
+  */
+  
+  /*
+    IDEAS:
+      Como los guerreros no pueden conquistar celdas ni gemas
+      Pueden protejer los ascencores o acompanyar al explorador
+      Dar ordenes de mas urgente a menos urgente ya que se ejecutan en orden
+  */
 
   /**
    * Types and attributes for your player can be defined here.
@@ -26,10 +57,7 @@ struct PLAYER_NAME : public Player {
    * Play method, invoked once per each round.
    */
   virtual void play () {
-    Dir d;
-    CellType c;
-    UnitType u;
-    Pos p(0,0,0);
+    
   }
 
 };

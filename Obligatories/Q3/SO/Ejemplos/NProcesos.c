561b4182425d949227c1c1e59be76311f93ae88c
d1mo22 <davidmc04and@gmail.com>
2023-12-21 23:30:45 +0100

    21/12/23

--- Obligatories/Q3/SO/Ejemplos/NProcesos.c
+++ Obligatories/Q3/SO/Ejemplos/NProcesos.c
@@ -3,23 +3,18 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void
-HacerTarea ()
-{
+void HacerTarea () {
   printf ("Mi PID es %d y el de mi padre %d\n", getpid (), getppid ());
 }
 
-void
-main (int argc, char *argv[])
-{
+void main (int argc, char *argv[]) {
   int i;
   int pid;
   printf ("Mi PID es %d\n", getpid ());
-  for (i = 0; i < 3; i++)
-    {
+  for (i = 0; i < 3; i++) {
       pid = fork ();
       if (pid == 0)
-	HacerTarea ();
-    }
+	    HacerTarea ();
+  }
   while (1);
 }

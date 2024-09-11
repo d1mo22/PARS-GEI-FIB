353d19924dceec2696ebbc43e6e8589dd66ae6c2
d1mo22 <davidmc04and@gmail.com>
2024-09-11 19:52:57 +0200

    haskell problems

--- Obligatories/Q5/LP/Jutge/Haskell/P29040/P29040.hs
+++ Obligatories/Q5/LP/Jutge/Haskell/P29040/P29040.hs
@@ -1,13 +1,53 @@
 insert :: [Int] -> Int -> [Int]
-insert [] x = [x]
+insert [] y = [y]
+insert (x : xs) y
+  | x > y = y : x : xs
+  | otherwise = x : insert xs y
 
--- isort :: [Int] -> [Int]
+isort :: [Int] -> [Int]
+isort [] = []
+isort (x : xs) = insert (isort xs) x
 
--- remove :: [Int] -> Int -> [Int]
--- ssort :: [Int] -> [Int]
+remove :: [Int] -> Int -> [Int]
+remove [] _ = []
+remove (x : xs) y
+  | y == x = xs
+  | otherwise = x : remove xs y
 
--- merge :: [Int] -> [Int] -> [Int]
--- msort :: [Int] -> [Int]
+ssort :: [Int] -> [Int]
+ssort [] = []
+ssort [x] = [x]
+ssort (x : xs)
+  | m < x = m : ssort (x : remove xs m)
+  | otherwise = x : ssort xs
+  where
+    m = minimum xs
 
--- qsort :: [Int] -> [Int]
--- genQsort :: Ord a => [a] -> [a]
+merge :: [Int] -> [Int] -> [Int]
+merge [] [] = []
+merge x [] = x
+merge [] x = x
+merge (x : xs) (y : ys)
+  | x > y = y : merge (x : xs) ys
+  | otherwise = x : merge xs (y : ys)
+
+msort :: [Int] -> [Int]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort left) (msort right)
+  where
+    (left, right) = splitAt (div (length xs) 2) xs
+
+qsort :: [Int] -> [Int]
+qsort [] = []
+qsort (p : xs) = qsort menors ++ [p] ++ qsort majors
+  where
+    menors = [x | x <- xs, x < p]
+    majors = [x | x <- xs, x >= p]
+
+genQsort :: (Ord a) => [a] -> [a]
+genQsort [] = []
+genQsort (p : xs) = genQsort menors ++ [p] ++ genQsort majors
+  where
+    menors = [x | x <- xs, x < p]
+    majors = [x | x <- xs, x >= p]

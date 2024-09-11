9075b9d649192b3316af490cc9a8a1c804cf45fe
d1mo22 <davidmc04and@gmail.com>
2024-09-11 19:18:20 +0200

    haskell problems

---
+++ Obligatories/Q5/LP/Jutge/Haskell/P29040/P29040.hs
@@ -0,0 +1,13 @@
+insert :: [Int] -> Int -> [Int]
+insert [] x = [x]
+
+-- isort :: [Int] -> [Int]
+
+-- remove :: [Int] -> Int -> [Int]
+-- ssort :: [Int] -> [Int]
+
+-- merge :: [Int] -> [Int] -> [Int]
+-- msort :: [Int] -> [Int]
+
+-- qsort :: [Int] -> [Int]
+-- genQsort :: Ord a => [a] -> [a]

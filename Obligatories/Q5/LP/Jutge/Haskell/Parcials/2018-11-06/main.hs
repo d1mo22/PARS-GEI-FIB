e678b60e6c3d8979bbac421b083854c167eee0ae
david.morais <david.morais@estudiantat.upc.edu>
2024-10-28 19:52:59 +0100

    haskell exam problems

---
+++ Obligatories/Q5/LP/Jutge/Haskell/Parcials/2018-11-06/main.hs
@@ -0,0 +1,63 @@
+----------------------------------------------------------- Romans
+value :: Char -> Int
+value 'I' = 1
+value 'V' = 5
+value 'X' = 10
+value 'L' = 50
+value 'C' = 100
+value 'D' = 500
+value 'M' = 1000
+
+-- I, II, III, IV, V, VI, VII, VIII, IX, X, XI
+
+roman2int :: String -> Int
+roman2int "" = 0
+roman2int [x] = value x
+roman2int (x : y : s)
+  | value x < value y = -value x + roman2int (y : s)
+  | otherwise = value x + roman2int (y : s)
+
+roman2int' :: String -> Int
+roman2int' s = snd $ foldr f (0, 0) s
+  where
+    f c (prev, acc)
+      | value c < prev = (value c, acc - value c)
+      | otherwise = (value c, acc + value c)
+
+----------------------------------------------------------- Taylor sqrt
+taylor :: Float -> Float -> Float
+taylor x tx = 0.5 * (tx + x / tx)
+
+arrels :: Float -> [Float]
+arrels x = iterate (taylor x) x
+
+diff :: Float -> (Float, Float) -> Bool
+diff e (n1, n2) = abs (n1 - n2) > e
+
+arrel :: Float -> Float -> Float
+arrel x e = snd (head (dropWhile (diff e) (zip (arrels x) (tail $ arrels x))))
+
+----------------------------------------------------------- Arbres
+data LTree a = Leaf a | Node (LTree a) (LTree a)
+
+instance (Show a) => Show (LTree a) where
+  show (Leaf x) = "{" ++ show x ++ "}"
+  show (Node a b) = "<" ++ show a ++ "," ++ show b ++ ">"
+
+build :: [a] -> LTree a
+build [x] = Leaf x
+build l = Node (build l1) (build l2)
+  where
+    len = div (length l + 1) 2
+    l1 = take len l
+    l2 = drop len l
+
+zipLTrees :: LTree a -> LTree b -> Maybe (LTree (a, b))
+zipLTrees (Node t1 t2) (Leaf y) = do Nothing
+zipLTrees (Leaf x) (Node t1 t2) = do Nothing
+zipLTrees (Leaf x) (Leaf y) = do
+  return (Leaf (x, y))
+zipLTrees (Node t1 t2) (Node t3 t4) = do
+  tx <- zipLTrees t1 t3
+  ty <- zipLTrees t2 t4
+  return (Node tx ty)
--- Obligatories/Q5/LP/Jutge/Haskell/Parcials/Nov 2023/P28687/main.hs
+++ Obligatories/Q5/LP/Jutge/Haskell/Parcials/Nov 2023/P28687/main.hs
@@ -1,11 +1,13 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Use camelCase" #-}
-
 import Data.Ratio
 
 termes_cosinus :: Rational -> [Rational]
-termes_cosinus x = (iterate (*) (-1)) * zipWith (/) (iterate (^ 2) 1) (scanl (*))
+termes_cosinus x = zipWith (*) (cycle [1, -1]) (zipWith (/) (map (x ^) [0, 2 ..]) factorialesPares)
 
 cosinus :: Rational -> Rational -> Rational
-cosinus x e = sum (takeWhile (>= e) termes_cosinus x)
+cosinus x n = sum (takeWhile (\term -> abs term >= n) (termes_cosinus x))
+
+factorialesPares :: [Rational]
+factorialesPares = map factorial [0, 2 ..]
+  where
+    factorial 0 = 1
+    factorial n = n * factorial (n - 1)
